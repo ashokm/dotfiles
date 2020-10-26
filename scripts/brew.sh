ef353d778263bfddd41b87cb84b823e7b33197b0
Ashok Manji <1902568+ashokm@users.noreply.github.com>
2020-10-26 10:43:49 +0100

    Uninstall pre-installed Homebrew on CI build agent

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -21,45 +21,23 @@ log() {
 install() {
   log "Install Homebrew"
   if test ! "$(command -v brew)"; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    HOMEBREW_INSTALL_URL="https://raw.githubusercontent.com/Homebrew/install/master/install.sh"
+    /bin/bash -c "$(curl -fsSL ${HOMEBREW_INSTALL_URL})"
   fi
 
-  # Check your Homebrew system for potential problems
-  if [[ -z "${CI_ENABLED}" ]]; then
-    echo "[INFO] Check your Homebrew system for potential problems ..."
-    brew cleanup
-    brew doctor
-  else
-    echo "[skip ci] Check your Homebrew system for potential problems ..."
-  fi
-
-  # Uninstall unwanted pre-installed packages on CI build agents
-  # See links below for installed software:
-  # https://github.com/microsoft/azure-pipelines-image-generation/tree/master/images
-  # https://github.com/actions/virtual-environments/tree/main/images
-  if [[ "${CI_ENABLED}" ]]; then
-    echo "[INFO] Uninstall unwanted pre-installed packages on CI build agents ..."
-    BREW_PACKAGES=('aws-sam-cli' 'session-manager-plugin')
-    for package in "${BREW_PACKAGES[@]}"; do
-      brew uninstall "$package" --force
-    done
-    FILES="/usr/local/bin/aws
-    /usr/local/aws-cli/aws
-    /usr/local/bin/aws_completer
-    /usr/local/aws-cli/aws_completer"
-    for file in $FILES; do
-      if [ -f "$file" ]; then
-        echo "[INFO] Removing $file ..."
-        sudo rm "$file"
-      fi
-    done
-  fi
+  echo "[INFO] Disable Homebrew analytics ..."
+  brew analytics off
 
   # Run Homebrew through the Brewfile
+  echo "[INFO] Uninstall packages not listed in Brewfile ..."
+  brew bundle cleanup --file="Brewfile" --force
   echo "[INFO] Install packages listed in Brewfile ..."
   brew bundle --file="Brewfile"
-  echo "[INFO] Uninstall packages not listed in Brewfile ..."
-  brew bundle cleanup --file="Brewfile" --force && brew cleanup
+
+  # Check your Homebrew system for potential problems
+  echo "[INFO] Check your Homebrew system for potential problems ..."
+  brew cleanup
+  brew doctor
 }
 
 uninstall() {
@@ -73,7 +51,8 @@ uninstall() {
       fi
       echo "[INFO] Uninstall packages installed using Brew ..."
       brew remove --force "$(brew list)" && brew cleanup
-      /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall.sh)"
+      HOMEBREW_UNINSTALL_URL="https://raw.githubusercontent.com/Homebrew/install/master/uninstall.sh"
+      /bin/bash -c "$(curl -fsSL ${HOMEBREW_UNINSTALL_URL})"
     fi
   else
     log "[skip ci] Uninstall Homebrew"
